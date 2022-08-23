c32287d54d35665d088360e110b45b54b2d0fc1a
hyeja <21800799@handong.edu>
2022-08-23 21:28:53 +0900

    검색바 및 테이블 헤더 추가

--- src/screens/AppMain.tsx
+++ src/screens/AppMain.tsx
@@ -10,9 +10,75 @@ import {
 import React, {useState} from 'react';
 import DatePicker from 'react-native-date-picker';
 import CalendarMonthIcon from 'react-native-vector-icons/MaterialIcons';
+import {Table, TableWrapper, Row, Rows} from 'react-native-table-component';
 const AppMain = ({}) => {
   const [date, setDate] = useState(new Date());
   const [open, setOpen] = useState(false);
+  const CONTENT = {
+    tableHead: [
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontSize: 12, fontWeight: 'bold'}}>번호</Text>
+      </View>,
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontSize: 12, fontWeight: 'bold'}}>제목</Text>
+      </View>,
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontSize: 12, fontWeight: 'bold'}}>작성자</Text>
+      </View>,
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontSize: 12, fontWeight: 'bold'}}>등록일</Text>
+      </View>,
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontSize: 12, fontWeight: 'bold'}}>조회건수</Text>
+      </View>,
+      <View style={{alignItems: 'center'}}>
+        <Text style={{fontSize: 12, fontWeight: 'bold'}}>메일발송그룹</Text>
+      </View>,
+      <View style={{alignItems: 'center', height: 25, marginTop: 10}}>
+        <Text style={{fontSize: 12, fontWeight: 'bold'}}>메일발송여부</Text>
+      </View>,
+    ],
+    tableData: [
+      [
+        <View
+          style={{
+            backgroundColor: '#ced6e0',
+            margin: 0,
+            height: 38,
+            alignItems: 'center',
+            alignContent: 'center',
+          }}>
+          <Text style={{marginTop: 8}}>등록일</Text>
+        </View>,
+
+        <TouchableOpacity style={{alignItems: 'flex-end'}}>
+          <CalendarMonthIcon
+            name="calendar-today"
+            size={20}
+            onPress={() => setOpen(true)}></CalendarMonthIcon>
+        </TouchableOpacity>,
+
+        <TouchableOpacity style={{alignItems: 'flex-end'}}>
+          <CalendarMonthIcon
+            name="calendar-today"
+            size={20}
+            onPress={() => setOpen(true)}></CalendarMonthIcon>
+        </TouchableOpacity>,
+        <View
+          style={{
+            backgroundColor: '#ced6e0',
+            margin: 0,
+            height: 38,
+            alignItems: 'center',
+            alignContent: 'center',
+          }}>
+          <Text style={{marginTop: 8}}>제목</Text>
+        </View>,
+        <TextInput style={{width: 1500}}></TextInput>,
+      ],
+    ],
+    row: {height: 220},
+  };
 
   function _onPressButton() {
     Alert.alert('You tapped the button!');
@@ -25,7 +91,7 @@ const AppMain = ({}) => {
       <View style={{alignItems: 'flex-end', marginRight: 10}}>
         <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
           <TouchableOpacity style={styles.button_1} onPress={_onPressButton}>
-            <Text>조회</Text>
+            <Text> 조회</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.button_2} onPress={_onPressButton}>
             <Text>신규등록</Text>
@@ -38,19 +104,18 @@ const AppMain = ({}) => {
           marginTop: 20,
           flexDirection: 'row',
           borderBottomColor: 'black',
-        }}>
-        <View style={{backgroundColor: 'lightgray', width: 50, height: 30}}>
-          <Text style={{textAlign: 'center'}}>등록일</Text>
-        </View>
-        <TextInput></TextInput>
-        <TouchableOpacity>
-          <CalendarMonthIcon
-            name="calendar-today"
-            size={20}
-            onPress={() => setOpen}></CalendarMonthIcon>
-        </TouchableOpacity>
-      </View>
-
+        }}></View>
+      <Table
+        borderStyle={{borderWidth: 1}}
+        style={{marginLeft: 10, marginRight: 10, height: 40}}>
+        <TableWrapper style={{alignContent: 'center'}}>
+          <Rows
+            widthArr={[60, 70, 70, 50, 122]}
+            data={CONTENT.tableData}
+            style={{alignContent: 'center'}}
+          />
+        </TableWrapper>
+      </Table>
       <DatePicker
         modal
         open={open}
@@ -63,6 +128,15 @@ const AppMain = ({}) => {
           setOpen(false);
         }}
       />
+      <Table
+        borderStyle={{borderWidth: 1}}
+        style={{marginTop: 50, marginLeft: 10, marginRight: 10}}>
+        <Row
+          style={{backgroundColor: 'lightgray'}}
+          widthArr={[35, 35, 45, 45, 52, 80, 80]}
+          data={CONTENT.tableHead}
+        />
+      </Table>
     </View>
   );
 };
